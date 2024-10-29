b2d87b97164644247d164892f0b778f84c44d017
Cade <johnston2@grinnell.edu>
2024-10-29 14:28:36 -0500

    Implement search method for iterables.

--- src/main/java/edu/grinnell/csc207/experiments/SearchExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/SearchExperiments.java
@@ -34,7 +34,33 @@ public class SearchExperiments {
                        "romeo", "sierra", "tango", "uniform",
                        "victor", "whiskey", "xray", "yankee", "zulu" };
     ArrayList<String> strings = new ArrayList<String>(Arrays.asList(tmp));
+    try {
+      String ex1c = SearchUtils.search(strings, (s) -> s.length() < 5);
+      pen.println("The first string of fewer than five letters is " + ex1c);
+    } catch (Exception e) {
+      pen.println("There are no strings of fewer than five letters.");
+    } // try/catch
 
+    try {
+      String ex1g = SearchUtils.search(strings, (s) -> s.length() == 6);
+      pen.println("The first string of exactly six letters is " + ex1g);
+    } catch (Exception e) {
+      pen.println("There are no strings of exactly six letters.");
+    } // try/catchS
+
+    try {
+      String ex1g = SearchUtils.search(strings, (s) -> s.contains("u"));
+      pen.println("The first string that contains u is " + ex1g);
+    } catch (Exception e) {
+      pen.println("There are no strings that contains u.");
+    } // try/catchS
+
+    // try {
+    //   String ex1g = SearchUtils.search(tmp, (s) -> s.contains("u"));
+    //   pen.println("The first string that contains u is " + ex1g);
+    // } catch (Exception e) {
+    //   pen.println("There are no strings that contains u.");
+    // } // try/catchS
     pen.close();
   } // main(String[])
 } // class SearchUtils
